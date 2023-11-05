021108b72973b27b93b532b00f8bc100655c89a6
Aayush <aayush.perecharla@infodatinc.com>
2023-11-05 09:56:54 -0600

    changed apiSlice

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://insurance-api.onrender.com' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://admin-panel-api-5r7s.onrender.com' }),
     tagTypes: ['Note', 'User'],
     endpoints: builder => ({})
 })
