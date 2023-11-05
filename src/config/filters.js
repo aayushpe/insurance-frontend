33ff320151a427fe5a1de6e78882686c9da8f385
Aayush <aayush.perecharla@infodatinc.com>
2023-11-05 01:29:30 -0600

    firstCommit

---
+++ src/config/filters.js
@@ -0,0 +1,6 @@
+export const FILTERS = {
+    PillBugInsurance: 'Pill-Bug Insurance',
+    RatInsurance: 'Rat Insurance',
+    ToiletInsurance: 'Toilet Insurance',
+    CatInsurance: 'Cat Insurance'
+}
